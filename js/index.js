5167697b60f4f7adfd9cc890e07e421b31511fe9
Jhonas Lalo <titojonesisreal@gmail.com>
2023-08-19 17:38:22 +0800

    added update functionality to subtitle in modal teksten

--- js/index.js
+++ js/index.js
@@ -11,7 +11,6 @@ const toolbarTrigger = () => {
       const targetContainer = document.querySelector(`.${toggleTarget}`);
 
       if (targetContainer) {
-        textEditoptions();
         toolbarListBtns.forEach((otherButton) => {
           if (otherButton !== button) {
             const otherToggleTarget =
@@ -30,6 +29,7 @@ const toolbarTrigger = () => {
         button.classList.toggle("active");
         targetContainer.style.display =
           targetContainer.style.display === "flex" ? "" : "flex";
+        textEditoptions();
       }
     });
   });
@@ -126,15 +126,12 @@ const textEditoptions = () => {
   const activeSecondbutton = (active, parentElement) => {
     if (active) {
       parentElement.style.borderStyle = "solid";
-      parentElement.innerHTML = `<h1 class="--eensubtitle">Een subtitel toevoegen</h1>
-      <button class="--plus-icon-container" style="background: #fff; opacity:1; pointer-events: auto">
-      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M16.1875 11.0312C16.5938 11.1562 16.8125 11.5625 16.6875 11.9688C15.8125 14.9375 13.0625 17 9.96875 17C7.5 17 5.21875 15.6562 3.96875 13.5625V16.75C3.96875 17.1875 3.65625 17.5 3.21875 17.5C2.8125 17.5 2.46875 17.1875 2.46875 16.75V11.75C2.46875 11.3438 2.8125 11 3.21875 11H8.21875C8.65625 11 8.96875 11.3438 8.96875 11.75C8.96875 12.1875 8.625 12.5 8.21875 12.5H5.0625C6 14.3125 7.875 15.5 9.96875 15.5C12.4062 15.5 14.5938 13.875 15.25 11.5312C15.375 11.125 15.7812 10.9062 16.1875 11.0312ZM16.75 2.5C17.1562 2.5 17.5 2.84375 17.5 3.25V8.25C17.5 8.6875 17.1562 9 16.75 9H11.75C11.3125 9 11 8.6875 11 8.25C11 7.84375 11.3125 7.5 11.75 7.5H14.875C13.9375 5.71875 12.0625 4.5 10 4.5C7.5625 4.5 5.375 6.15625 4.6875 8.46875C4.59375 8.875 4.1875 9.09375 3.78125 8.96875C3.375 8.875 3.15625 8.4375 3.25 8.0625C4.125 5.09375 6.90625 3 10 3C12.4688 3 14.75 4.375 16 6.46875V3.25C16 2.84375 16.3125 2.5 16.75 2.5Z" fill="#2280D2"/>
-      </svg>
-      
-      </button>`;
+      parentElement.style.justifyContent = "center";
 
-      const newButton = parentElement.querySelector(".--plus-icon-container");
+      parentElement.innerHTML = `<h1 class="--eensubtitle">Een subtitel toevoegen</h1>
+    `;
+
+      const newButton = parentElement.querySelector(".--eensubtitle");
       secondbuttonlistener(newButton);
     } else {
       parentElement.style.borderStyle = "dashed";
@@ -159,37 +156,78 @@ const textEditoptions = () => {
       secondbuttonlistener(newButton);
     }
   };
-  const secondbuttonlistener = (button) => {
-    button.addEventListener("click", (event) => {
-      event.preventDefault();
-      const parentElement = button.closest(".--teksten-button-holder");
-      if (parentElement) {
-        parentElement.classList.toggle("active");
-        const active = parentElement.classList.contains("active");
-        activeSecondbutton(active, parentElement);
-      }
+  const triggerinputstyle = (getInput) => {
+    getInput.pointerEvents = "auto";
+    getInput.style.border = "1px solid red";
+    getInput.addEventListener("blur", () => {
+      getInput.style.border = "none";
     });
+  };
+  const secondbuttonlistener = (button) => {
+    if (button !== null) {
+      if (button.classList.contains("--eensubtitle")) {
+        button.addEventListener("click", (event) => {
+          event.preventDefault();
+          const getInput = document.querySelector(".inputsubtitle");
+          triggerinputstyle(getInput);
+          getInput.focus();
+          activeSecondbutton(getInput);
+        });
+      } else {
+        button.addEventListener("click", (event) => {
+          event.preventDefault();
+
+          const parentElement = button.closest(".--teksten-button-holder");
+
+          if (parentElement) {
+            parentElement.classList.toggle("active");
+            const active = parentElement.classList.contains("active");
+            activeSecondbutton(active, parentElement);
+          } else {
+            console.log("wala");
+          }
+        });
+      }
+    }
   };
   hasactiveContainer.forEach((buttons) => {
     const isActive = buttons.classList.contains("active");
     const toggleTarget = buttons.getAttribute("data-toggle-target");
-
+    const subtitel = document.querySelector(".--eensubtitle");
     if (toggleTarget === "--free-made-teksten") {
       pointertrue(isActive);
 
       h1editable.addEventListener("click", () => {
+        if (subtitel !== null) {
+          subtitel.style.pointerEvents = "auto";
+          subtitel.style.cursor = "pointer";
+        }
         textoptionbuttons.forEach((button, index) => {
           activePointer(button);
+
           if (index === 0) {
             firstbuttonlistener(button);
           }
-          if (index === 1) {
+          if (
+            index === 1 &&
+            button.parentElement &&
+            button.parentElement.id === "secondbtn"
+          ) {
             secondbuttonlistener(button);
+          }
+          if (index === 2) {
+            console.log("wala");
           }
         });
       });
       hasActiveclass(isActive);
     } else {
+      const subtitel = document.querySelector(".--eensubtitle");
+      if (subtitel !== null) {
+        console.log(subtitel);
+        subtitel.style.pointerEvents = "none";
+        subtitel.style.cursor = "context-menu";
+      }
       textoptionbuttons.forEach((button) => {
         button.style.opacity = "0.3";
         button.style.pointerEvents = "none";
@@ -225,7 +263,7 @@ const stiljfontChange = () => {
         }
         if (index === 1) {
           const middlecontent = document.querySelectorAll(
-            ".--middletext-content p, .--lowertext-content p, .--upper-text p"
+            ".--middletext-content p, .--lowertext-content p, .--upper-text p, .--middletext-content input::placeholder,.--middletext-content input  "
           );
 
           middlecontent.forEach((transform) => {
